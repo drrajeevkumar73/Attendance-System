d6458e7cd00d277a6ab3c92f93567c7201838546
dr rajeev kumar <abhihomeohall72@gmail.com>
2025-01-17 11:54:26 +0530

    la

--- src/app/api/pr/[username]/route.ts
+++ src/app/api/pr/[username]/route.ts
@@ -4,10 +4,22 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(
   request: NextRequest,
-  { params }: { params: Params },
+  { params }: { params: Params }
 ) {
   try {
-    const { username }: any = params;
+    const { username } = params;
+
+    // Ensure username is a valid string
+    if (!username || Array.isArray(username)) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Invalid username parameter.",
+        },
+        { status: 400 }
+      );
+    }
+
     const decodedUsername = decodeURIComponent(username);
 
     const res = await prisma.user.findFirst({
@@ -35,7 +47,7 @@ export async function POST(
         success: false,
         message: "Internal server error.",
       },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
